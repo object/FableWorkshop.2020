a37f2f0d79a1c1a12a673c5807e6279c87ef0690
Vagif Abilov <vagif.abilov@nrk.no>
2020-10-15 22:16:58 +0200

    Completed 6 step.

--- src/Client/View.fs
+++ src/Client/View.fs
@@ -1,27 +1,64 @@
 module View
 
+open Feliz
+
 open System
 open Model
 open Messages
 
-open Fable.React
-open Fable.React.Props
-
 let view (state : Model) dispatch =
-    div [ Style [ TextAlign TextAlignOptions.Center; Padding 40 ] ] [
-        div [] [
-            img [ Src "favicon.png" ]
-            h1 [] [ str "File Player" ]
-            div [ClassName "control"] [input [OnChange (fun evt -> dispatch (FilenameChanged evt.Value))]]
-            div [] [
-                span [] [ str "Playback delay:" ]
-                span [ClassName "control"] [input [Value (sprintf "%d" state.PlaybackDelay); OnChange (fun evt -> dispatch (PlaybackDelayChanged evt.Value))]]
+
+    let showPlayOrPauseButton () =
+        if state.IsPlaying 
+        then Html.button [
+            prop.disabled (String.IsNullOrEmpty state.Filename)
+            prop.onClick (fun _ -> dispatch PausePlayback)
+            prop.text "Pause" ]
+        else Html.button [
+            prop.disabled (String.IsNullOrEmpty state.Filename)
+            prop.onClick (fun _ -> dispatch StartPlayback)
+            prop.text "Play" ]
+
+    let showStopButton () =
+        Html.button [
+            prop.disabled (String.IsNullOrEmpty state.Filename || not state.IsPlaying)
+            prop.onClick (fun _ -> dispatch StopPlayback)
+            prop.text "Stop" ]
+
+    Html.div [ 
+        prop.style [ style.alignContent.center ; style.padding 40 ]
+        prop.children [
+            Html.div [
+                prop.children [
+                    Html.img [prop.src "favicon.png"]
+                    Html.h1 [prop.text "File Loader"]
+                    Html.div [
+                        prop.className "control"
+                        prop.children [
+                            Html.input [
+                                prop.onChange (FilenameChanged >> dispatch)
+                            ]
+                        ]
+                    ]
+                    Html.div [
+                        prop.children [
+                            Html.span [ prop.text "Playback speed:" ]
+                            Html.span [
+                                prop.className "control"
+                                prop.children [
+                                    Html.input [
+                                        prop.value (sprintf "%d" state.PlaybackDelay)
+                                        prop.onChange (PlaybackDelayChanged >> dispatch)
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                    showPlayOrPauseButton ()
+                    showStopButton ()
+                    Html.p [prop.text (if Array.isEmpty state.Events || state.EventIndex < 0 then "" else state.Events.[state.EventIndex])]
+                    Html.p [prop.text state.Error]
+                ]
             ]
-            if state.IsPlaying 
-            then button [Disabled (String.IsNullOrEmpty state.Filename); OnClick (fun _ -> dispatch PausePlayback)] [str "Pause"]
-            else button [Disabled (String.IsNullOrEmpty state.Filename); OnClick (fun _ -> dispatch StartPlayback)] [str "Play"]
-            button [Disabled (String.IsNullOrEmpty state.Filename || not state.IsPlaying); OnClick (fun _ -> dispatch StopPlayback)] [str "Stop"]
-            p [] [ str <| if Array.isEmpty state.Events || state.EventIndex < 0 then "" else state.Events.[state.EventIndex] ]
-            p [] [ str state.Error ]
         ]
     ]
